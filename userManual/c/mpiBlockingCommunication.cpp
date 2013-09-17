8b88cfe6ef0fe538d65d5c5d757bf131a6185246
Kelly Black <kjblack@gmail.com>
2013-09-17 14:39:54 -0400

    Updated the basic exmaple for blocking communication.

--- userManual/c/mpiBlockingCommunication.cpp
+++ userManual/c/mpiBlockingCommunication.cpp
@@ -5,74 +5,89 @@
 // mpic++ -o  mpiManagementExample mpiManagementExample.cpp 
 // mpirun -np 4 --host localhost mpiManagementExample
 
+// The size of the array to be sent to the next process.
 #define NUMBER 10
 
 int main(int argc,char **argv)
 {
-	int mpiResult;
-	int numProcesses;
-	int numtasks;
-	int rank;
-	char hostname[MPI_MAX_PROCESSOR_NAME];
-	int len;
+  // MPI job information.
+  int  mpiResult;     // Used to check the results from MPI library calls
+  int  numtasks;      // Total number of processes spawned for this job.
+  int  rank;          // The unique number associated with this process.
 
-	double val[NUMBER];
-	int lupe;
-	MPI_Status theStatus;
+  // Information about the host this process is running on.
+  char hostname[MPI_MAX_PROCESSOR_NAME]; 
+  int len;
 
-	mpiResult = MPI_Init (&argc,&argv);
-	if(mpiResult!= MPI_SUCCESS)
-		{
-			std::cout << "MPI not started. Terminating the process." << std::endl;
-			MPI_Abort(MPI_COMM_WORLD,mpiResult);
-		}
+  // Information to be shared between processes.
+  double val[NUMBER];
+  int lupe;
+  MPI_Status theStatus;
 
-	MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
-	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-	MPI_Get_processor_name(hostname, &len);
-	//std::cout << "Number of tasks= " <<  numtasks
-	//					<< " My rank= " << rank
-	//					<< " Running on " << hostname
-	//					<< std::endl;
+  // Initialize the session
+  mpiResult = MPI_Init (&argc,&argv);
+  if(mpiResult!= MPI_SUCCESS)
+    {
+      std::cout << "MPI not started. Terminating the process." << std::endl;
+      MPI_Abort(MPI_COMM_WORLD,mpiResult);
+    }
 
-	// Initialize the buffer
-	for(lupe=0;lupe<NUMBER;++lupe)
-		{
-			val[lupe] = (double)lupe;
-		}
 
-	if(rank == 0)
-		{
-			// This is the first process.
-			std::cout << "Sending message from the zero process" << std::endl;
-			MPI_Send(val,NUMBER,MPI_DOUBLE,1,10,MPI_COMM_WORLD);
-			std::cout << "Zero process waiting to hear the message from " << numtasks-1 << std::endl;
-			MPI_Recv(val,NUMBER,MPI_DOUBLE,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&theStatus);
-			std::cout << "Zero process heard from  " << theStatus.MPI_SOURCE << " with tag " 
-								<< theStatus.MPI_TAG << std::endl;
-		}
-	else
-		{
-			// Wait to hear the message.
-			std::cout << "Process " << rank << " waiting to hear the message" << std::endl;
-			MPI_Recv(val,NUMBER,MPI_DOUBLE,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&theStatus);
-			std::cout << "Process " << rank << " heard from  " << theStatus.MPI_SOURCE << " with tag " 
-								<< theStatus.MPI_TAG << std::endl;
+  // Get information about this session and this process 
+  MPI_Comm_size(MPI_COMM_WORLD,&numtasks);  // get the number of processes
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);      // get the rank of this process
+  MPI_Get_processor_name(hostname, &len);   // Get the host name for
+                                            // this process
 
-			// update what was sent.
-			for(lupe=0;lupe<NUMBER;++lupe)
-				val[lupe] += 1.0;
 
-			// pass it along to the next process
-			std::cout << "Process " << rank << " sending message to " << (rank+1)%numtasks << " process." << std::endl;
-			MPI_Send(val,NUMBER,MPI_DOUBLE,(rank+1)%numtasks,10,MPI_COMM_WORLD);
-		}
+  // Decide to start the communication or wait to hear something.
+  if(rank == 0)
+    {
+      // This is the first process. Send my information to the second process.
 
-	// print out what was passed
-	std::cout << "Process " << rank << " heard: ";
-	for(lupe=0;lupe<NUMBER;++lupe)
-		std::cout << val[lupe] << ", ";
-	std::cout << std::endl;
+      // First initialize the buffer
+      for(lupe=0;lupe<NUMBER;++lupe)
+        {
+          val[lupe] = (double)lupe;
+        }
 
-	MPI_Finalize();
+      // Now pass the information along to the next process.
+      std::cout << "Sending message from the zero process" << std::endl;
+      MPI_Send(val,NUMBER,MPI_DOUBLE,1,10,MPI_COMM_WORLD);
+
+      // Now wait to get the information from the last process in the ring.
+      std::cout << "Zero process waiting to hear the message from " << numtasks-1 << std::endl;
+      MPI_Recv(val,NUMBER,MPI_DOUBLE,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&theStatus);
+      std::cout << "Zero process heard from  " << theStatus.MPI_SOURCE << " with tag " 
+                << theStatus.MPI_TAG << std::endl;
+    }
+
+  else
+    {
+      // I am not the first process. Wait until I get the information
+      // before passing along what I have.
+      std::cout << "Process " << rank << " waiting to hear the message" << std::endl;
+      MPI_Recv(val,NUMBER,MPI_DOUBLE,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&theStatus);
+      std::cout << "Process " << rank << " heard from  " << theStatus.MPI_SOURCE << " with tag " 
+                << theStatus.MPI_TAG << std::endl;
+
+      // update the information that was sent to me.
+      for(lupe=0;lupe<NUMBER;++lupe)
+        val[lupe] += 1.0;
+
+      // Finally pass it along to the next process in the ring.
+      std::cout << "Process " << rank << " sending message to " 
+                << (rank+1)%numtasks << " process." << std::endl;
+      MPI_Send(val,NUMBER,MPI_DOUBLE,(rank+1)%numtasks,10,MPI_COMM_WORLD);
+    }
+
+  // print out the information that I have.
+  std::cout << "Process " << rank << " heard: ";
+  for(lupe=0;lupe<NUMBER;++lupe)
+    std::cout << val[lupe] << ", ";
+  std::cout << std::endl;
+
+  // All done. Time to wrap it up.
+  MPI_Finalize();
+  return(0);
 }
