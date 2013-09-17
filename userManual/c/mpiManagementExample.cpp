ff87ed27abd87d504e48967a5cdcf45e49b636b0
Kelly Black <kjblack@gmail.com>
2013-09-17 14:29:18 -0400

    Added comments to the basic mpi example.

--- userManual/c/mpiManagementExample.cpp
+++ userManual/c/mpiManagementExample.cpp
@@ -7,27 +7,36 @@
 
 int main(int argc,char **argv)
 {
-	int mpiResult;
-	int numProcesses;
-	int numtasks;
-	int rank;
-	char hostname[MPI_MAX_PROCESSOR_NAME];
-	int len;
+  // MPI job information.
+  int  mpiResult;     // Used to check the results from MPI library calls
+  int  numtasks;      // Total number of processes spawned for this job.
+  int  rank;          // The unique number associated with this process.
 
-	mpiResult = MPI_Init (&argc,&argv);
-	if(mpiResult!= MPI_SUCCESS)
-		{
-			std::cout << "MPI not started. Terminating the process." << std::endl;
-			MPI_Abort(MPI_COMM_WORLD,mpiResult);
-		}
+  // Host Information 
+  char hostname[MPI_MAX_PROCESSOR_NAME];
+  int  len;
 
-	MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
-	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-	MPI_Get_processor_name(hostname, &len);
-	std::cout << "Number of tasks= " <<  numtasks
-						<< " My rank= " << rank
-						<< " Running on " << hostname
-						<< std::endl;
+  // Initialize the session
+  mpiResult = MPI_Init (&argc,&argv);
+  if(mpiResult!= MPI_SUCCESS)
+    {
+      std::cout << "MPI not started. Terminating the process." << std::endl;
+      MPI_Abort(MPI_COMM_WORLD,mpiResult);
+    }
 
-	MPI_Finalize();
+  // Get information about this session and this process 
+  MPI_Comm_size(MPI_COMM_WORLD,&numtasks);  // get the number of processes
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);      // get the rank of this process
+  MPI_Get_processor_name(hostname, &len);   // Get the host name for
+                                            // this process
+
+  // Print out the information about this process.
+  std::cout << "Number of tasks= " <<  numtasks
+            << " My rank= " << rank
+            << " Running on " << hostname
+            << std::endl;
+
+  // All done. Time to wrap it up.
+  MPI_Finalize();
+  return(0);
 }
