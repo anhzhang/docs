637003fa5b6fd6bc691a3ff27ad36d4e17159d42
Kelly Black <kjblack@gmail.com>
2013-09-17 16:44:04 -0400

    Updated and added the example for asynchronous communications.

--- userManual/c/mpiNonblockingCommunication.cpp
+++ userManual/c/mpiNonblockingCommunication.cpp
@@ -5,72 +5,83 @@
 // mpic++ -o  mpiManagementExample mpiManagementExample.cpp 
 // mpirun -np 4 --host localhost mpiManagementExample
 
+// The size of the array to be sent to the next process.
 #define NUMBER 10
 
 int main(int argc,char **argv)
 {
-	int mpiResult;
-	int numProcesses;
-	int numtasks;
-	int rank;
-	char hostname[MPI_MAX_PROCESSOR_NAME];
-	int len;
+  // MPI job information.
+  int  mpiResult;     // Used to check the results from MPI library calls
+  int  numtasks;      // Total number of processes spawned for this job.
+  int  rank;          // The unique number associated with this process.
 
-	double val[NUMBER];
-	int lupe;
-	MPI_Status  theStatus[2];
-	MPI_Request theRequests[2];
+  // Information about the host this process is running on.
+  char hostname[MPI_MAX_PROCESSOR_NAME];
+  int len;
 
-	int test1;
-	int test2;
+  // Information to be shared between processes.
+  double val[NUMBER];
+  int lupe;
 
-	mpiResult = MPI_Init (&argc,&argv);
-	if(mpiResult!= MPI_SUCCESS)
-		{
-			std::cout << "MPI not started. Terminating the process." << std::endl;
-			MPI_Abort(MPI_COMM_WORLD,mpiResult);
-		}
+  // Variables to keep track of the status and the requests to send
+  // info.
+  MPI_Status  theStatus[2];
+  MPI_Request theRequests[2];
 
-	MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
-	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-	MPI_Get_processor_name(hostname, &len);
-	//std::cout << "Number of tasks= " <<  numtasks
-	//					<< " My rank= " << rank
-	//					<< " Running on " << hostname
-	//					<< std::endl;
+  // Variables used to determine if the info has come through.
+  int test1;
+  int test2;
 
-	// Initialize the buffer
-	for(lupe=0;lupe<NUMBER;++lupe)
-		{
-			val[lupe] = (double)lupe + rank;
-		}
+  // Initialize the session
+  mpiResult = MPI_Init (&argc,&argv);
+  if(mpiResult!= MPI_SUCCESS)
+    {
+      std::cout << "MPI not started. Terminating the process." << std::endl;
+      MPI_Abort(MPI_COMM_WORLD,mpiResult);
+    }
 
-	// pass the info along to the next process
-	std::cout << "Process " << rank << " sending message to " << (rank+1)%numtasks << " process." << std::endl;
-	MPI_Isend(val,NUMBER,MPI_DOUBLE,(rank+1)%numtasks,10,MPI_COMM_WORLD,&theRequests[0]);
+  // Get information about this session and this process 
+  MPI_Comm_size(MPI_COMM_WORLD,&numtasks);  // get the number of processes
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);      // get the rank of this process
+  MPI_Get_processor_name(hostname, &len);   // Get the host name for
+                                            // this process
 
-	// Wait to hear the message.
-	std::cout << "Process " << rank << " waiting to hear the message" << std::endl;
-	MPI_Irecv(val,NUMBER,MPI_DOUBLE,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&theRequests[1]);
+  // Initialize the buffer to be sent.
+  for(lupe=0;lupe<NUMBER;++lupe)
+    {
+      val[lupe] = (double)lupe + rank;
+    }
 
-	// keep checking to see when everything is done.
-	// This is not a good way to do it, and it is only for demonstration purposes.
-	test1 = 0;
-	test2 = 0;
-	while(!test1 && !test2)
-		{
-			MPI_Test (&theRequests[0],&test1,&theStatus[0]);
-			MPI_Test (&theRequests[1],&test2,&theStatus[1]);
-		}
 
-	// better yet just wait for them all at once....
-	MPI_Waitall(2, theRequests, theStatus);
+  // pass the info along to the next process
+  std::cout << "Process " << rank << " sending message to " 
+            << (rank+1)%numtasks << " process." << std::endl;
+  MPI_Isend(val,NUMBER,MPI_DOUBLE,(rank+1)%numtasks,10,MPI_COMM_WORLD,&theRequests[0]);
 
-	// print out what was passed
-	std::cout << "Process " << rank << " heard: ";
-	for(lupe=0;lupe<NUMBER;++lupe)
-		std::cout << val[lupe] << ", ";
-	std::cout << std::endl;
+  // Wait to hear the message.
+  std::cout << "Process " << rank << " waiting to hear the message" << std::endl;
+  MPI_Irecv(val,NUMBER,MPI_DOUBLE,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&theRequests[1]);
 
-	MPI_Finalize();
+  // keep checking to see when everything is done.
+  // This is not a good way to do it, and it is only for demonstration purposes.
+  test1 = 0;
+  test2 = 0;
+  while(!test1 && !test2)
+    {
+      MPI_Test (&theRequests[0],&test1,&theStatus[0]);
+      MPI_Test (&theRequests[1],&test2,&theStatus[1]);
+    }
+
+  // Alternatively, just wait for them all at once....
+  MPI_Waitall(2, theRequests, theStatus);
+
+  // print out the info that was passed
+  std::cout << "Process " << rank << " heard: ";
+  for(lupe=0;lupe<NUMBER;++lupe)
+    std::cout << val[lupe] << ", ";
+  std::cout << std::endl;
+
+  // All done. Time to wrap it up.
+  MPI_Finalize();
+  return(0);
 }
